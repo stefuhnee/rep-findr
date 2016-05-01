7b1b3e44d4a093772cf2ce00e3e9084fd80a1b9e
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-01 16:54:21 -0700

    server config

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT=8080;
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
 var Twitter = require('twitter');
