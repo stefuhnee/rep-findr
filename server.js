05962be42e7d9733c0bb2e58da041b7fadc55fb8
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-01 16:30:39 -0700

    stuff

--- server.js
+++ server.js
@@ -19,10 +19,21 @@ client.stream('statuses/filter', {track: 'republican'}, function(stream) {
 
 client.stream();
 
+
 var requestProxy = require('express-request-proxy'),
   express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
+
+var proxyGitHub = function(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+  }))(request, response);
+};
+//
+// app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 
