8560b3a21535d0e3c555794f98f81de8ff720cec
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-01 09:54:32 -0700

    sample server file reinitialized

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var google = require('googleapis');
+
+var proxyGitHub = function(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+  }))(request, response);
+};
+
+app.get('/github/*', proxyGitHub);
+
+app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
