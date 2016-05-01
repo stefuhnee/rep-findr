d431572a52401f28358de2628aecc6759dfbb7bf
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-01 09:53:03 -0700

    trying with matts call

--- server.js
+++ server.js
@@ -1,5 +1,1 @@
 var google = require('googleapis');
-// var auth = GOOGLE_TOKEN;
-// var urlshortener = google.urlshortener({ version: 'v1', auth: auth });
-googleapis.discover('civicinfo', 'v2');
-  // make requests here
