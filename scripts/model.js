fcccef613e23481e4e57c8820e869a25e11fe025
MatthewAllanWilson <mattwils@gmail.com>
2016-05-02 16:14:26 -0700

    object constructor now working

--- scripts/model.js
+++ scripts/model.js
@@ -4,12 +4,22 @@
 
   civicDataAPI.info = {};
 
+  function Official(data) {
+    var sen1 = data.officials[0];
+    var sen2 = data.officials[1];
+    this.name1 = sen1.name;
+    this.address1 = sen1.address[0];
+    this.phone1 = sen1.phones[0];
+    this.name2 = sen2.name;
+    this.address2 = sen2.address[0];
+    this.phone2 = sen2.phones[0];
+  }
+
   civicDataAPI.requestData = function(userAddress, official) {
     $.get('https://www.googleapis.com/civicinfo/v2/representatives?address=' + userAddress + '&includeOffices=true&levels=country&roles=' + official + '&key=AIzaSyBe_nzIg-0E_xI5V8owjDT_we48Xp0psPk')
     .done(function(data) {
-      civicDataAPI.info = data;
+      civicDataAPI.info = new Official(data);
       console.table(civicDataAPI.info);
-      // return civicData;
     });
   };
 
