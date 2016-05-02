93c2789943ddaa72dce565757d28c75987718340
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-01 18:20:02 -0700

    twitter feed is working in node, unable to grab data. leveraged .env node library to easily store/access env variables

--- civic.js
+++ civic.js
@@ -7,3 +7,19 @@ function requestData () {
    console.log(civicData);
  });
 };
+
+$("#feedTarget").twitterFeed({
+		        count: 4,
+                rawData: yourRawJSONData,
+                prepend: "<div class='tweetWrapper'>",
+                append: "</div>",
+                tweetBodyClass: "tweetBody tweetText",
+		        date: { prepend: "<div>", append: " - ", order: 3, cssClass: "tweetDate" },
+		        retweet: { show: false },
+		        favorite: { prepend: " - ", order: 0, append: "</div>" },
+		        callbackOnEach: true,
+		        callback: function() {
+                $(this).find(".tweetBody").myCallbackOnEachTweet();
+            }
+	      });
+    });
