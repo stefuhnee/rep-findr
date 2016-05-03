2ba1447d84ac93f365f8303c8c66bebf9df23aba
MatthewAllanWilson <mattwils@gmail.com>
2016-05-03 14:59:01 -0700

    drop box location rendered onto map correctly

--- scripts/model.js
+++ scripts/model.js
@@ -28,16 +28,16 @@
 
   civicDataAPI.initialize = function() {
     geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng();
+    var latlng = new google.maps.LatLng(122.332, -47.606);
     var mapOptions = {
-      zoom: 8,
+      zoom: 15,
       center: latlng
     }
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
-  civicDataAPI.requestMap = function () {
-    geocoder.geocode( { 'address': userAddress}, function(results, status) {
+  civicDataAPI.requestMap = function (boxAddress) {
+    geocoder.geocode( { 'address': boxAddress}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
@@ -50,11 +50,15 @@
     });
   }
 
-  civicDataAPI.requestDropBox = function(userAddress) {console.log(userAddress);
+  civicDataAPI.requestDropBox = function(userAddress) {
+    // console.log(userAddress);
     $.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=' + userAddress + '&fields=dropOffLocations&key=AIzaSyBe_nzIg-0E_xI5V8owjDT_we48Xp0psPk')
     .done(function(data) {
       civicDataAPI.dropbox = data;
-      console.table(civicDataAPI.dropbox);
+      // console.table(civicDataAPI.dropbox);
+      var pollAddress = civicDataAPI.dropbox.dropOffLocations[0].address.line1 + ' ' + civicDataAPI.dropbox.dropOffLocations[0].address.city + ' ' + civicDataAPI.dropbox.dropOffLocations[0].address.state + ' ' + civicDataAPI.dropbox.dropOffLocations[0].address.zip;
+      console.log(pollAddress);
+      civicDataAPI.requestMap(pollAddress);
     });
   };
 
