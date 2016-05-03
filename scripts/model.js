efa43747aa63cee6f2b19a90100a02652b545546
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-03 10:36:34 -0700

    set up basic server

--- scripts/model.js
+++ scripts/model.js
@@ -16,7 +16,7 @@
   }
 
   civicDataAPI.requestData = function(userAddress, official) {
-    $.get('https://www.googleapis.com/civicinfo/v2/representatives?address=' + userAddress + '&includeOffices=true&levels=country&roles=' + official + '&key=AIzaSyBe_nzIg-0E_xI5V8owjDT_we48Xp0psPk')
+    $.get('https://www.googleapis.com/civicinfo/v2/representatives?address=' + userAddress + '&includeOffices=true&levels=country&roles=' + official + '&key=' + GOOGLE_CIVIC_KEY)
     .done(function(data) {
       civicDataAPI.info = new Official(data);
       console.table(civicDataAPI.info);
