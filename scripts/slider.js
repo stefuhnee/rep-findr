3bb1e408b516196a71aa927749f2ac0ca6b0dc55
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-05 15:10:15 -0700

    added elephant and donkey photos

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-  $('.slider').bxSlider( {
-    auto: true,
-    autoControls: false,
-    randomStart: true,
-    controls: false
-  });
-});
+// $(document).ready(function() {
+//   $('.slider').bxSlider( {
+//     auto: true,
+//     autoControls: false,
+//     randomStart: true,
+//     controls: false
+//   });
+// });
