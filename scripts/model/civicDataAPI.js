f62a3e6f3976333895e5e78f27cd0e19affc2c55
MatthewAllanWilson <mattwils@gmail.com>
2016-05-05 14:03:36 -0700

    deleted console logs and commented out code

--- scripts/model/civicDataAPI.js
+++ scripts/model/civicDataAPI.js
@@ -19,7 +19,6 @@
     $.get('https://www.googleapis.com/civicinfo/v2/representatives?address=' + userAddress + '&includeOffices=true&levels=' + level + '&roles=' + official + '&key=AIzaSyBe_nzIg-0E_xI5V8owjDT_we48Xp0psPk')
     .success(function(data) {
       civicDataAPI.handleData(data);
-      console.log('data returned and passed to handleData', data)
     });
   };
 
--- server.js
+++ server.js
@@ -6,10 +6,8 @@ var express = require('express'),
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
-  console.log('New request:', request.url);
   response.sendFile('index.html', { root: '.' });
 });
 
 app.listen(port, function() {
-  console.log('Server started on port ' + port + '!');
 });
