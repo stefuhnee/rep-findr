f450c9a9f3ef520e26aa4b1a96a886eeb32b1c86
MatthewAllanWilson <mattwils@gmail.com>
2016-05-05 17:50:01 -0700

    comment added on javascript

--- scripts/model/dataBase.js
+++ scripts/model/dataBase.js
@@ -4,6 +4,7 @@
 
   dataBase.myDataBaseRef = new Firebase('https://rep-findr.firebaseio.com/');
 
+  //You found our easter egg!
   dataBase.easterEgg = new Konami(function() {
     alert('Trump is president!  We\'re doomed.');
   });
